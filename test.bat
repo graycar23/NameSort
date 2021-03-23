b3558b7ff3e405f78093cf9e754bd261497a7c09
Grayson Salyers <57331411+graycar23@users.noreply.github.com>
2021-03-23 13:25:07 -0400

    Delete test.bat

--- test.bat
+++
@@ -1,7 +0,0 @@
-NameSort.exe -s < "Sort Me.txt" > SortOutput.txt
-fc Sorted.txt SortOutput.txt
-
-NameSort.exe -r < "Sort Me.txt" > ReverseOutput.txt
-fc ReverseSorted.txt ReverseOutput.txt
-
-pause
