4285567f702ed0a6b9a173fc9bcb390a2d3c0a5d
Grayson Salyers <57331411+graycar23@users.noreply.github.com>
2021-03-28 17:13:34 -0400

    Create NameSort.sh

---
+++ NameSortTest/NameSort.sh
@@ -0,0 +1,7 @@
+bash NameSort.exe -s < "Sort Me.txt" > SortOutput.txt
+cmp Sorted.txt SortOutput.txt
+
+bash NameSort.exe -r < "Sort Me.txt" > ReverseOutput.txt
+cmp ReverseSorted.txt ReverseOutput.txt
+
+sleep 1
