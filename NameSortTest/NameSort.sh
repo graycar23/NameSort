98e16ab9fb092c5d519f43c8b3bef386cb8ba9fd
Grayson Salyers <57331411+graycar23@users.noreply.github.com>
2021-03-28 17:27:14 -0400

    Update NameSort.sh

--- NameSortTest/NameSort.sh
+++ NameSortTest/NameSort.sh
@@ -1,7 +1,9 @@
-$ bash ./NameSort.exe -s < "Sort Me.txt" > SortOutput.txt
+#! /bin/bash
+
+$ ./NameSort.exe -s < "Sort Me.txt" > SortOutput.txt
 cmp Sorted.txt SortOutput.txt
 
-$ bash ./NameSort.exe -r < "Sort Me.txt" > ReverseOutput.txt
+$ ./NameSort.exe -r < "Sort Me.txt" > ReverseOutput.txt
 cmp ReverseSorted.txt ReverseOutput.txt
 
 sleep 1
