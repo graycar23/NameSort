5d542d7e9e9f9992fd70b6fd059da237f22566fa
Grayson Salyers <57331411+graycar23@users.noreply.github.com>
2021-03-29 00:23:01 -0400

    Update NameSort.sh

--- NameSortTest/NameSort.sh
+++ NameSortTest/NameSort.sh
@@ -8,7 +8,7 @@ mcs -out:NameSort1.exe Program.cs
 chmod u+x NameSort1.exe
 
 mono NameSort1.exe -s < "Sort Me.txt" > SortOutput.txt
-diff -u <( tr -d '^M' <Sorted.txt ) SortOutput.txt | cat -t
+diff -u <( tr -d '\r' <Sorted.txt ) SortOutput.txt | cat -t
 
 mono NameSort1.exe -r < "Sort Me.txt" > ReverseOutput.txt
-diff -u <( tr -d '^M' <ReverseSorted.txt ) ReverseOutput.txt | cat -t
+diff -u <( tr -d '\r' <ReverseSorted.txt ) ReverseOutput.txt | cat -t
