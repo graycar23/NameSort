10dcbb2305465ef857e4d71791013662481eb892
Grayson Salyers <57331411+graycar23@users.noreply.github.com>
2021-03-29 00:17:01 -0400

    Update NameSort.sh

--- NameSortTest/NameSort.sh
+++ NameSortTest/NameSort.sh
@@ -9,8 +9,8 @@ chmod u+x NameSort1.exe
 
 mono NameSort1.exe -s < "Sort Me.txt" > SortOutput.txt
 dos2unix Sorted.txt
-diff -u Sorted.txt SortOutput.txt | cat -t
+diff -u <( tr -d '^M' <Sorted.txt ) SortOutput.txt | cat -t
 
 mono NameSort1.exe -r < "Sort Me.txt" > ReverseOutput.txt
 dos2unix ReverseSorted.txt
-diff -u ReverseSorted.txt ReverseOutput.txt | cat -t
+diff -u <( tr -d '^M' <ReverseSorted.txt ) ReverseOutput.txt | cat -t
